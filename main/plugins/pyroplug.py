68a0bcfe51673c4a337f89bc48be544ebb36d34a
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-03-22 17:33:41 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -74,7 +74,7 @@ async def send_video_with_chat_id(client, sender, path, caption, duration, hi, w
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading: [Team SPY](https://t.me/dev_gagan)__**\n ',
+                '**__Uploading: [˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)__**\n ',
                 upm,
                 time.time()
             )
@@ -97,7 +97,7 @@ async def send_document_with_chat_id(client, sender, path, caption, thumb_path, 
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading:__**\n**__Bot made by [Team SPY](https://t.me/dev_gagan)__**',
+                '**__Uploading:__**\n**__Bot made by [˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)__**',
                 upm,
                 time.time()
             )
@@ -256,7 +256,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     logging.info(e)
                     thumb_path = None
                 
-                caption = f"{msg.caption}\n\n__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__" if msg.caption else "__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__"
+                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__"
                 await send_video_with_chat_id(client, sender, path, caption, duration, hi, wi, thumb_path, upm)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 if file_n != '':
@@ -270,7 +270,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
 
                 
-                caption = f"{msg.caption}\n\n__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__" if msg.caption else "__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__"
+                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__"
                 await upm.edit("__Uploading photo...__")
 
                 await bot.send_file(sender, path, caption=caption)
@@ -286,7 +286,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
                 thumb_path=thumbnail(sender)
                 
-                caption = f"{msg.caption}\n\n__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__" if msg.caption else "__Unrestricted by **[Team SPY](https://t.me/dev_gagan)**__"
+                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__"
                 await send_document_with_chat_id(client, sender, path, caption, thumb_path, upm)
             os.remove(file)
             await upm.delete()
