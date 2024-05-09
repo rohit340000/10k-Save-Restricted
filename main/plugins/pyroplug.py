060531562ecbabbfa7a31dd52d99970e4419b5a1
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-05-09 07:19:20 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -256,7 +256,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     logging.info(e)
                     thumb_path = None
                 
-                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__"
+                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__"
                 await send_video_with_chat_id(client, sender, path, caption, duration, hi, wi, thumb_path, upm)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 if file_n != '':
@@ -270,7 +270,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
 
                 
-                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__"
+                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__"
                 await upm.edit("__Uploading photo...__")
 
                 await bot.send_file(sender, path, caption=caption)
@@ -286,7 +286,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                     file = path
                 thumb_path=thumbnail(sender)
                 
-                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/jaiavsjqhab)**__"
+                caption = f"{msg.caption}\n\n__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__" if msg.caption else "__Download by **[˙⁠❥⁠˙ℝ𝕆ℍ𝕀𝕋˙⁠❥˙⁠](https://t.me/galib_shayar)**__"
                 await send_document_with_chat_id(client, sender, path, caption, thumb_path, upm)
             os.remove(file)
             await upm.delete()
